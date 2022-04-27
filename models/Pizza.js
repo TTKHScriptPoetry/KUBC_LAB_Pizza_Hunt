5d12b63352e404676ed86315a481cae21453402f
= <=>
2022-04-26 23:28:40 -0500

    Add comment module having virtual properties comment count

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,46 @@
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
-   pizzaName: {
-     type: String
-   },
-   createdBy: {
-     type: String
-   },
-   createdAt: {
-     type: Date,
-     default: Date.now
-   },
-   size: {
-     type: String,
-     default: 'Large'
-   },
-   toppings: []
- });
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: [],
+  comments: [
+      { 
+        type: Schema.Types.ObjectId, // type to be an ObjectId >> so that Mongoose knows to expect a comment?
+        ref: 'Comment' // Tells the Pizza model which documents to search to find the right comments.
+      }
+    ] // The type of String is just a placeholder. We'll need to update it to refer to the Comment type that we've created.
+  }, // end of 1st obj
+  {
+    // -- Tell the schema that it can use virtuals
+    toJSON: { 
+      virtuals: true,
+    },
+    id: false // False because it is a virtual that Mongoose returns, and we don’t need it.
+  }
+);
+
+ // -- Virtuals allow you to add virtual properties to a document that aren't stored  
+ // -- in the database before responding to the API request: total count of comments 
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+// // So that we can do:
+// const pizza = await Pizza.findOne()
+// pizza.commentCount // 5
 
  // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
